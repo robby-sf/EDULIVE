21454a7321512145d61dcb8d97cba56b317028c3
RavelinLuth <ravelinlutfhanrlsp@gmail.com>
2025-07-13 17:32:26 +0700

    finish header (before auth)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/header', function () {
+    return view('header');
+});
+
+
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
