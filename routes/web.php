96c728adfa4be9dac14a0f150e12cd3829b26f5a
RavelinLuth <ravelinlutfhanrlsp@gmail.com>
2025-07-13 17:54:32 +0700

    recovery

--- routes/web.php
+++ routes/web.php
@@ -7,14 +7,12 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+Route::get('/header', function () {
+    return view('header');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/login', function () {
+    return view('auth.login');
+})->name('login');
+
+// indo
