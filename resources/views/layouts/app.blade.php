94ecbc811efc643da8e205ca18718b7f6faf9781
RavelinLuth <ravelinlutfhanrlsp@gmail.com>
2025-07-19 22:33:54 +0700

    adding favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,7 @@
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 
     {{-- Link Font & Ikon bisa tetap ada --}}
+    <link rel="icon" href="{{ asset('favicon.png') }}" type="image/x-icon">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet">
