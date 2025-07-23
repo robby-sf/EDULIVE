3f4f02e3c9b469028d86c5a797e70a0c1b30bd28
robby-sf <robbyc@gmail.com>
2025-07-23 23:29:30 +0700

    Revert "Merge branch 'dev' into tes"
    This reverts commit 059e8cf0ca3b85ef482b0f625cc0c844bd591589, reversing
    changes made to 1b6b18131c88cf9e2f0ea4d8110a0da5e0ae40a6.

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -25,9 +25,7 @@ class StudyController extends Controller
             'ended_at' => 'required|date',
             'focus_duration' => 'required|numeric',
             'distraction_duration' => 'required|numeric',
-            'distractions' => 'nullable|array',
             'distraction_log' => 'required|array',
-
         ]);
 
         Log::info($data);
