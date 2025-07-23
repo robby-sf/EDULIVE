9294fe54d1ef3867553a6401be7b447af020481e
RifqiMakarim <rifqimakarim8@gmail.com>
2025-07-23 23:12:46 +0700

    fix bug

--- app/Http/Controllers/StudyController.php
+++ app/Http/Controllers/StudyController.php
@@ -25,8 +25,8 @@ class StudyController extends Controller
             'ended_at' => 'required|date',
             'focus_duration' => 'required|numeric',
             'distraction_duration' => 'required|numeric',
-            'distractions' => 'nullable|array',
-            'distraction_log' => 'required|array',
+            // 'distractions' => 'nullable|array',
+            'distraction_log' => 'nullable|array',
 
         ]);
 
